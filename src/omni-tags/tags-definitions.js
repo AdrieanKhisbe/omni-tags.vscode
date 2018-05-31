80eda28625e2d391efec47217f8d43afb12d3090
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-05-31 09:35:23 +0200

    proto: attempt to replace the look behind negative match

--- src/omni-tags/tags-definitions.js
+++ src/omni-tags/tags-definitions.js
@@ -3,7 +3,7 @@ const secondarySymbol = '¤';
 const tertiarySymbol = '※';
 
 const symbolRegex = `[${primarySymbol}${secondarySymbol}${tertiarySymbol}]`;
-const simpleTagRegex = new RegExp(`${symbolRegex}\\w[\\w-:]*[!\\?¡¿]*(?<!:)(?=\\s|$)`);
+const simpleTagRegex = new RegExp(`${symbolRegex}(?!.*:(\\s|$))\\w[\\w-:]*[!\\?¡¿]*(?=\\s|$)`);
 
 
 module.exports = {
