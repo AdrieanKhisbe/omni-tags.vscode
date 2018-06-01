d563d1c0d843c90c3ae7a993d90d0fafe44cc548
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-06-02 00:57:35 +0200

    proto: implement a go to previous tag

--- src/omni-tags/navigation.js
+++ src/omni-tags/navigation.js
@@ -1,32 +1,67 @@
 const { symbolRegex } = require('./tags-definitions');
 
-module.exports = function () {
-    const vscode = require('vscode')
+module.exports = function (vscode) {
+
+    const tagSymbol = new RegExp(`${symbolRegex.source}+`, 'g');
+    const startWithTag = new RegExp(`^${symbolRegex.source}`);
+
     const gotoNextTag = (n = 1) => {
-        const tagSymbol = new RegExp(`${symbolRegex.source}+`, 'g')
-        const startWithTag = new RegExp(`^${symbolRegex.source}`)
         const editor = vscode.window.activeTextEditor;
         const document = editor.document;
         const lastLine = document.lineCount - 1;
         const forwardRange = new vscode.Range(editor.selection.anchor.line, editor.selection.anchor.character,
             lastLine, document.lineAt(lastLine).range.end.character)
         const text = document.getText(forwardRange);
-        let match = tagSymbol.exec(text);
+
+
         const moveToTag = match => {
             const tagPosition = document.positionAt(document.offsetAt(editor.selection.start) + match.index);
             editor.selection = new vscode.Selection(tagPosition, tagPosition);
             return tagPosition;
         }
-        if (startWithTag.test(text)) {
-            console.log('tsrta with tag')
-            match = tagSymbol.exec(text)
-        }
-        if (match) {
-            const initialPosition = editor.selection.anchor;
-            const newPosition = moveToTag(match);
+        let i = 0, match;
+        if (startWithTag.test(text)) match = tagSymbol.exec(text);
+        while (i < n) {
+            match = tagSymbol.exec(text);
+
+            if (match) {
+                moveToTag(match);
+            } else break;
+            i++;
         }
     }
+    const gotoPreviousTag = (n = 1) => {
+        try{
+        const editor = vscode.window.activeTextEditor;
+        const document = editor.document;
+        console.log(document.lineAt(0).range)
+        console.log(editor.selection.anchor.character)
+        const backwardRange = new vscode.Range(0, document.lineAt(0).range.start.character,
+            editor.selection.anchor.line, editor.selection.anchor.character)
+        const text = document.getText(backwardRange);
+
+        const moveToTag = match => {
+            const tagPosition = document.positionAt(match.index);
+            editor.selection = new vscode.Selection(tagPosition, tagPosition);
+            return tagPosition;
+        }
+        const matchStack = [];
+        let match, i = 0;
+        while (match = tagSymbol.exec(text)) matchStack.push(match);
+
+        if (startWithTag.test(text)) matchStack.pop();
+        while (i < n) {
+            const backwardMatch = matchStack.pop()
+            if (backwardMatch) moveToTag(backwardMatch);
+            else break;
+            i++;
+        }
+    }catch(err){
+        console.error(err)
+    }
+    }
     return {
-        gotoNextTag
+        gotoNextTag,
+        gotoPreviousTag
     };
 }
