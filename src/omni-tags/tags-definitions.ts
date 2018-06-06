e493bc9cc2d4946f4c743ae5a7dc4097f07a4977
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-06-06 07:34:50 +0200

    fix: improve grammar and match regexp to grab simple tags on same line that detailed tags

--- src/omni-tags/tags-definitions.ts
+++ src/omni-tags/tags-definitions.ts
@@ -3,6 +3,6 @@ export const secondarySymbol = '¤';
 export const tertiarySymbol = '※';
 
 export const symbolRegex = new RegExp(`[${primarySymbol}${secondarySymbol}${tertiarySymbol}]`);
-export const simpleTagRegex = new RegExp(`${symbolRegex.source}+(?!.*:(\\s|$))\\w[\\w-:]*[!\\?¡¿]*(?=\\s|$)`);
-export const keywordTagRegex = new RegExp(`(${symbolRegex.source}+)(?!.*:(\\s|$))(\\w[\\w-:]*)(?=\\s|$|:)`);
+export const simpleTagRegex = new RegExp(`${symbolRegex.source}+(?!\\S*:(\\s|$))\\w[\\w-:]*[!\\?¡¿]*(?=\\s|$)`);
+export const keywordTagRegex = new RegExp(`(${symbolRegex.source}+)(?!\\S*:(\\s|$))(\\w[\\w-:]*)(?=\\s|$|:)`);
 export const detailTagRegex = new RegExp(`${symbolRegex.source}+\\w[\\w-:]*:\\s(\\\\['"\`]|[^'"\`])*(?=\\z|$)`);
