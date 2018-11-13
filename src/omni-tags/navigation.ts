da0f5a0be4f8ee0f906ee8fdcbaf4698526d2a79
AdrieanKhisbe <adriean.khisbe@live.fr>
2018-11-13 23:52:44 +0100

    proto: further refactor of navigation functions

--- src/omni-tags/navigation.ts
+++ src/omni-tags/navigation.ts
@@ -5,73 +5,67 @@ export interface NavigationArg {
     n?: number;
 }
 
-const moveToTag = (document: vscode.TextDocument, editor: vscode.TextEditor, match: RegExpExecArray) => {
-    const tagPosition = document.positionAt(document.offsetAt(editor.selection.start) + match.index);
-    editor.selection = new vscode.Selection(tagPosition, tagPosition);
-    return tagPosition;
-};
-
 export const gotoNextTag = (args: NavigationArg) => {
     const n = args && args.n || 1;
-    const tagSymbol = new RegExp(`${symbolRegex.source}+`, 'g');
-    const startWithTag = new RegExp(`^${symbolRegex.source}`);
     const editor = vscode.window.activeTextEditor;
-    if(!editor) return;
+    if (!editor) return;
     const document = editor.document;
     const lastLine = document.lineCount - 1;
     const forwardRange = new vscode.Range(editor.selection.anchor.line, editor.selection.anchor.character,
         lastLine, document.lineAt(lastLine).range.end.character)
     const text = document.getText(forwardRange);
 
+    const tagSymbol = new RegExp(`${symbolRegex.source}+`, 'g');
+    const startWithTag = new RegExp(`^${symbolRegex.source}`);
     let i = 0, match;
     if (startWithTag.test(text)) match = tagSymbol.exec(text);
     while (i < n) {
         match = tagSymbol.exec(text);
-
-        if (match) {
-            moveToTag(document, editor, match);
-        } else {
-            vscode.window.showWarningMessage('No further tags ahead');
-            break;
-        }
         i++;
     }
-    vscode.commands.executeCommand("revealLine", {
-        lineNumber: editor.selection.start.line
-    });
-}
+    console.log(match)
+    if (match) { // move to tag
+        const tagPosition = document.positionAt(document.offsetAt(editor.selection.start) + match.index);
+        editor.selection = new vscode.Selection(tagPosition, tagPosition);
+        vscode.commands.executeCommand("revealLine", {
+            lineNumber: editor.selection.start.line
+        });
+    } else {
+        vscode.window.showWarningMessage('No further tags ahead');
+    }
+};
 
 
 export const gotoPreviousTag = (args: NavigationArg) => {
     const n = args && args.n || 1;
-    const tagSymbol = new RegExp(`${symbolRegex.source}+`, 'g');
-    const startWithTag = new RegExp(`^${symbolRegex.source}`);
     const editor = vscode.window.activeTextEditor;
-    if(!editor) return;
+    if (!editor) return;
     const document = editor.document;
     const backwardRange = new vscode.Range(0, document.lineAt(0).range.start.character,
         editor.selection.anchor.line, editor.selection.anchor.character)
     const text = document.getText(backwardRange);
 
+    const tagSymbol = new RegExp(`${symbolRegex.source}+`, 'g');
+    const startWithTag = new RegExp(`^${symbolRegex.source}`);
     const matchStack = [];
-    let match, i = 0;
+    let match, i = 0, backwardMatch;
     while (match = tagSymbol.exec(text)) matchStack.push(match);
 
     if (startWithTag.test(text)) matchStack.pop();
     while (i < n) {
-        const backwardMatch = matchStack.pop()
-        if (backwardMatch) moveToTag(document, editor, backwardMatch);
-        else {
-            vscode.window.showWarningMessage('No further tags behind');
-            break;
-        }
+        backwardMatch = matchStack.pop();
         i++;
     }
-
-    vscode.commands.executeCommand("revealLine", {
-        lineNumber: editor.selection.start.line
-    });
-}
+    if (backwardMatch) {
+        const tagPosition = document.positionAt(backwardMatch.index);
+        editor.selection = new vscode.Selection(tagPosition, tagPosition);
+        vscode.commands.executeCommand("revealLine", {
+            lineNumber: editor.selection.start.line
+        });
+    } else {
+        vscode.window.showWarningMessage('No further tags behind');
+    }
+};
 /*
 Code for similar tag:
     const position = editor.selection.active;
@@ -82,4 +76,4 @@ Code for similar tag:
 
 export default {
     gotoNextTag, gotoPreviousTag
-}
+};
