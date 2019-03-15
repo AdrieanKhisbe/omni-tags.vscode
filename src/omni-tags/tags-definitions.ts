cd7b3c04002ffad5a46c1195279fcb773d06a963
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-03-15 08:22:22 +0100

    proto: fix outline tags symbols via keywordTagRegex

--- src/omni-tags/tags-definitions.ts
+++ src/omni-tags/tags-definitions.ts
@@ -5,4 +5,4 @@ export const tertiarySymbol = '※';
 export const symbolRegex = new RegExp(`[${primarySymbol}${secondarySymbol}${tertiarySymbol}]`);
 export const simpleTagRegex = new RegExp(`${symbolRegex.source}+(?!\\S*:(\\s|$))\\w[\\w-:]*[!\\?¡¿]*(?=\\s|$)`);
 export const detailTagRegex = new RegExp(`${symbolRegex.source}+\\w[\\w-:]*:\\s(\\\\['"\`]|[^'"\`])*(?=\\z|$)`);
-export const keywordTagRegex = new RegExp(`(${symbolRegex.source}+)(\\w[\\w-:\\?¡¿]*)(?=\\s|$|:|${symbolRegex.source})`);
+export const keywordTagRegex = new RegExp(`(${symbolRegex.source}+)(\\w[\\w-:\\?!¡¿]*)(?=\\s|$|: |\\b)`);
