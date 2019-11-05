feac79b21e25a115fad2f7b76a01e150d3ca375d
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-11-06 00:22:21 +0100

    Convert webpack config to typescript too :sparkles:

---
+++ webpack.config.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import {Configuration} from 'webpack';
+
+const config: Configuration = {
+    target: 'node',
+    entry: './src/omni-tags/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'omni-tags.js',
+        libraryTarget: 'commonjs2',
+        devtoolModuleFilenameTemplate: '../[resource-path]'
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: 'commonjs vscode'
+    },
+    resolve: {extensions: ['.ts', '.js']},
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [{loader: 'ts-loader'}]
+            }
+        ]
+    }
+};
+module.exports = config;
